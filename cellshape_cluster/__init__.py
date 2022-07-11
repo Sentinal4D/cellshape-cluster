18771b55c535c1a4ddd97a4eeefc535e3c0a7a04
DeVriesMatt <matthew.devries.13@gmail.com>
2022-07-11 15:21:31 +0100

    Bump version: 0.0.10 → 0.0.11-rc0

--- cellshape_cluster/__init__.py
+++ cellshape_cluster/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.10"
+__version__ = "0.0.11-rc0"
 
 from cellshape_cluster.deep_embedded_clustering import DeepEmbeddedClustering
 from cellshape_cluster.training_functions import train
