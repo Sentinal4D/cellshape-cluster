73ac541cd29926f15211d773241f8cc838cee73d
DeVriesMatt <matthew.devries.13@gmail.com>
2022-07-12 17:19:58 +0100

    Bump version: 0.0.13-rc0 → 0.0.13

--- cellshape_cluster/__init__.py
+++ cellshape_cluster/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.13-rc0"
+__version__ = "0.0.13"
 
 from cellshape_cluster.deep_embedded_clustering import DeepEmbeddedClustering
 from cellshape_cluster.training_functions import train
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [
 
 setup(
     name="cellshape-cluster",
-    version="0.0.13-rc0",
+    version="0.0.13",
     description="3D shape analysis using deep learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
