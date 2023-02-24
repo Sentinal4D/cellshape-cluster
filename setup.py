7168ed59f075671236370a31759d29ca732f28f3
DeVriesMatt <matthew.devries.13@gmail.com>
2023-02-24 11:25:15 +0000

    Bump version: 0.0.18-rc0 → 0.0.18

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [
 
 setup(
     name="cellshape-cluster",
-    version="0.0.18-rc0",
+    version="0.0.18",
     description="3D shape analysis using deep learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
