c452851c94d2db75f8573fc7fe59a118aa26224a
DeVriesMatt <matthew.devries.13@gmail.com>
2023-04-27 15:04:34 +0100

    update to fix bugs

--- cellshape_cluster/reports.py
+++ cellshape_cluster/reports.py
@@ -4,9 +4,9 @@ import logging
 
 
 def get_model_name(model):
-    encoder_type = model.encoder_type
-    decoder_type = model.decoder_type
-    num_features = model.encoder.num_features
+    encoder_type = model.autoencoder.encoder_type
+    decoder_type = model.autoencoder.decoder_type
+    num_features = model.autoencoder.encoder.num_features
     model_name = f"{encoder_type}_{decoder_type}_{num_features}_pretrained"
     return model_name
 
