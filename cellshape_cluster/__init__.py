c45783f587dcf347d2162f34ca8a59c8578f281a
DeVriesMatt <matthew.devries.13@gmail.com>
2023-04-27 14:52:53 +0100

    update init

--- cellshape_cluster/__init__.py
+++ cellshape_cluster/__init__.py
@@ -3,9 +3,11 @@ __version__ = "0.0.18"
 from .deep_embedded_clustering import DeepEmbeddedClustering
 from .clustering_layer import ClusteringLayer
 from .training_functions import train
+from .reports import get_experiment_name
 
 __all__ = (
     "DeepEmbeddedClustering",
     "ClusteringLayer",
     "train",
+    "get_experiment_name",
 )
